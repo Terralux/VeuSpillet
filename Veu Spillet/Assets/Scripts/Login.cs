e91e81556bf043e3c7c60203b8eaf82527ca6854
Terralux <kenneth@berle.eu>
2017-05-02 14:06:19 +0200

    Added changes

--- Veu Spillet/Assets/Scripts/Login.cs
+++ Veu Spillet/Assets/Scripts/Login.cs
@@ -16,12 +16,14 @@ public class Login : MonoBehaviour {
 
 	// Use this for initialization
 	IEnumerator Start () {
-		
-		string URL = "http://veuspillet.dk/getuser.php";
+
+		string URL = "http://veuspillet.dk/getAllUsers.php";
+		//string URL = "http://veuspillet.dk/getuser.php";
+
 		WWW ItemsData = new WWW (URL);
 		yield return ItemsData;
 		string dataString = ItemsData.text;
-		users = dataString.Split('#');
+		users = dataString.Split('|');
 
 		string[] segmentedUserData;
 		userIDs = new string[users.Length-1];
